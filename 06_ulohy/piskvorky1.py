20fd7e742fcddfbf4dc73518c9093730a8666e4c
Jana Ranková <rankova.jana@gmail.com>
2019-11-09 18:21:56 +0100

    Opravena verzia prvej casti piskvoriek.

--- 06_ulohy/piskvorky1.py
+++ 06_ulohy/piskvorky1.py
@@ -2,11 +2,12 @@ hracie_pole = ['-' * 20]
 
 def vyhodnot(hracie_pole):
     """Vezme momentalny stav hracieho pola a podla podmienok vo funkcii, urci vyhercu."""
-    if 'xxx' in hracie_pole == True:
+    stav = ''
+    if 'xxx' in hracie_pole[0]:
         stav = 'x'
-    elif 'ooo' in hracie_pole == True:
+    elif 'ooo' in hracie_pole[0]:
         stav = 'o'
-    elif '-' in hracie_pole == False:
+    elif '-' not in hracie_pole[0]:
         stav = '!'
     else:
         stav = '-'
